2de32a893959b064a016972c31592b368b835d11
Dhairya Patel <dhairya00798@gmail.com>
2022-02-20 09:26:53 +0530

    build folder added

---
+++ build/precache-manifest.5fa1c9fe71d18fef7ce05c04e99b3c38.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a18eacd03d5ba7c6372fd14bf77a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd7538341d8d2faaa6b",
+    "url": "/static/css/2.f0a32705.chunk.css"
+  },
+  {
+    "revision": "644ff9f1a73f04b39522",
+    "url": "/static/css/main.72db3361.chunk.css"
+  },
+  {
+    "revision": "ffd7538341d8d2faaa6b",
+    "url": "/static/js/2.4352e175.chunk.js"
+  },
+  {
+    "revision": "3a2a69c791665c00d3572e0db27bc703",
+    "url": "/static/js/2.4352e175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644ff9f1a73f04b39522",
+    "url": "/static/js/main.ca149928.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f116e1ef405932f14cede81029e90ef5",
+    "url": "/static/media/CCLOGO.f116e1ef.png"
+  },
+  {
+    "revision": "3f04485607fd816367dc466fa846f853",
+    "url": "/static/media/case.3f044856.png"
+  },
+  {
+    "revision": "376f543e29e8a7c4a3879a336aad39ed",
+    "url": "/static/media/challenger-cpu.376f543e.png"
+  },
+  {
+    "revision": "e6d8d2302f71a6144b896189eb291cb1",
+    "url": "/static/media/empty.e6d8d230.png"
+  },
+  {
+    "revision": "3a5738ef4f0230d8324cb86f2501960c",
+    "url": "/static/media/footerlogo-challenger.3a5738ef.png"
+  },
+  {
+    "revision": "baf78c18e630a33d24f996eae1a30d75",
+    "url": "/static/media/gameselectionPC.baf78c18.svg"
+  },
+  {
+    "revision": "43208743519aaa206566a96aef86555a",
+    "url": "/static/media/geforce.43208743.jpg"
+  },
+  {
+    "revision": "0520acb6db9704337b82ce2f92dda574",
+    "url": "/static/media/logo.0520acb6.jpeg"
+  },
+  {
+    "revision": "586bdf812e69703dfec6a3d8af873182",
+    "url": "/static/media/man.586bdf81.png"
+  },
+  {
+    "revision": "bc6ca634f21ffdc5cf8d496e3da97e2d",
+    "url": "/static/media/meditation.bc6ca634.png"
+  },
+  {
+    "revision": "0129fb09a490cf04a94fd38e986d5a7a",
+    "url": "/static/media/navlogo-challenger.0129fb09.png"
+  },
+  {
+    "revision": "3f96b0d99ce77e14e4c21a25738d045e",
+    "url": "/static/media/order.3f96b0d9.gif"
+  },
+  {
+    "revision": "0835d8fefc008bda7b1edb67c49b71a7",
+    "url": "/static/media/pc_3d.0835d8fe.gif"
+  },
+  {
+    "revision": "d8249e80cf91d33886a5237e0ac6ccbe",
+    "url": "/static/media/redopt.d8249e80.gif"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ac950421087f15cf85a55dfb1a6d052c",
+    "url": "/static/media/revieww.ac950421.png"
+  },
+  {
+    "revision": "4d2783547ff66b09fc21df7384c38498",
+    "url": "/static/media/sad.4d278354.png"
+  },
+  {
+    "revision": "78c138f75a2bf76d809f30d12bd3c11a",
+    "url": "/static/media/searching.78c138f7.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "793c8d81fb8415ff11081157a9712ef3",
+    "url": "/static/media/storeicon.793c8d81.svg"
+  },
+  {
+    "revision": "06d660ef27b04c48b95f3eccfa4c71de",
+    "url": "/static/media/whatsection.06d660ef.gif"
+  }
+]);
